c5ea0cbd0d482bea384993c3c9f9d558dd922c1c
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2026-01-22 18:59:58 +0100

    Update scripts, add yay  installing script

---
+++ scripts/dev-yay.sh
@@ -0,0 +1,1 @@
+yay -S --noconfirm --needed waybar zen-browser-bin oh-my-posh linux-wallpaperengine-git fnm sddm-silent-theme papirus-icon-theme vicinae-bin
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --needed bat btop gimp lsd tldr fzf ghostty
+sudo pacman -S --noconfirm --needed git base-devel
+
+sudo pacman -S --noconfirm --needed bat btop gimp lsd tldr fzf ghostty hyprpolkitagent wl-clipboard hyprlauncher zsh neovim stow fastfetch fortune-mod hyprpicker hyprlock ttf-jetbrains-mono-nerd qt5ct qt6ct nwg-look pavucontrol fd pnpm docker bluez luarocks bluez-utils blueman
+
+# waybar prerequisities
+sudo pacman -S --noconfirm --needed \
+  gtkmm3 \
+  jsoncpp \
+  libsigc++ \
+  fmt \
+  chrono-date \
+  spdlog \
+  gobject-introspection \
+  libmpdclient \
+  sndio \
+  meson \
+  cmake \
+  scdoc \
+  glib2-devel
